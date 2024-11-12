c059af14bbff0c0d5278944d8dff84ed1b2075b5
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2024-11-12 14:16:04 -0300

    Delete dia01/listarItem.py

--- dia01/listarItem.py
+++
@@ -1,6 +0,0 @@
-def listarItem(lista_itens):
-
-    print("\n----------- LISTA DE ITENS ----------\n")
-    print("PRODUTO\t\t\tQUANTIDADE")
-    for i in lista_itens:
-        print(f"{i['nome']}\t\t\t{i['quantidade']}")
